6ff466ec56d18c5115f81cfca54c0cdbeb5fd2f3
dev-rafael92as <77762757+dev-rafael92as@users.noreply.github.com>
2021-11-03 01:12:28 -0300

    Aula 13 - Criação de Aplicações e Sistemas

---
+++ 1º Semestre/Criação de Aplicações e Sistemas/MeuPrimeiroProgramaJava.java
@@ -0,0 +1,22 @@
+// Salvar como MeuPrimeiroProgramaJava.java
+
+//nome da classe
+class MeuPrimeiroProgramaJava
+{
+  //Módulo principal com a entrada pela linha de comando
+  public static void main(String entrada[])
+  {
+    //declaração de variáveis
+      int inteiro = 28;
+      char caracter = 'M';
+      double real = 1.70;
+      String frase = "Rafael Barros ";
+      boolean VF = true;
+
+      if (VF == true)
+      {
+        System.out.printIn("Eu sou o " + frase + "tenho " + inteiro + " anos e tenho " + real + "m de altura");
+      }
+      System.exit(0);
+  }
+}
